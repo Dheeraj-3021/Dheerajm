f166fa3b3d836384abf874025f86b84c5286b153
Dheeraj M <159900713+Dheeraj-3021@users.noreply.github.com>
2024-02-13 17:55:32 +0530

    Create Statistics.cpp

---
+++ Statistics.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    int c;
+    cin>>a>>b>>c;
+    cout<<"1)Dead:"<<a;
+    cout<<"\n"<<"2)Injured:"<<b;
+    cout<<"\n"<<"3)Safe:"<<c;
+    cout<<"\n"<<"Please help the people who are suffering!!!";
+    return 0;
+}
